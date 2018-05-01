d174aff9326548d7d09b598c39a4b535f8b3cb13
Krithicka Sivarajan <krithisowb@gmail.com>
2018-05-01 22:25:00 +0530

    Modified C++ solution to Java solution

--- Solve Me First/Solution.java
+++ Solve Me First/Solution.java
@@ -1,13 +1,26 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-int solveMeFirst(int a, int b) {
- return a+b;
-}
-int main() {
-  int sum;
-  cout<<sum;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+    static int solveMeFirst(int a, int b) {
+      return a+b;
+      
+   }
+
+   
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = solveMeFirst(a, b);
+        System.out.println(sum);
+   }
 }
